050727b15b819562b90e2495c89c39f442d79453
Zachary Priddy <github@zpriddy.com>
2019-11-15 18:07:26 -0800

    Move it

--- docs-src/conf.py
+++ docs-src/conf.py
@@ -61,7 +61,7 @@ html_theme_options = {
     'collapse_navigation': False
 }
 
-github_url="https://github.com/zpriddy/GLaDOS"
+github_url="https://github.com/zpriddy/GLaDOS/html"
 
 apidoc_module_dir = "../glados"
 apidoc_separate_modules = True
