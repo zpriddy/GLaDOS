9d3a612bc207d08c9d89f90d944bf6e4516746ed
Zach Priddy <zach.priddy@getcruise.com>
2020-02-05 10:55:15 -0800

    Update change in tests

--- tests/test_glados_core.py
+++ tests/test_glados_core.py
@@ -6,5 +6,5 @@ def test_glados_import_bots(caplog):
     g = Glados(GLADOS_CONFIG_FILE)
     g.read_config()
     g.import_bots()
-    assert len(g.bots) == 3
+    assert len(g.bots) == 4
     assert sorted(list(g.bots.keys())) == SORTED_BOT_NAMES
